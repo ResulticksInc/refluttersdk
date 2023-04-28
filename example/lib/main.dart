68b1c6d0d0c20f7bfcfc7abe962e1d7061dceed5
Vishwa KS <zenvishwa@gmail.com>
2023-04-28 09:01:28 +0530

    replaced existing code with cleanedup code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,71 +1,79 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:refluttersdk/refluttersdk.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
-import 'package:refluttersdk/refluttersdk.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:get/get.dart';
-import 'package:uni_links/uni_links.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyAppState extends State<MyApp> {
-
-  bool _initialURILinkHandled = false;
-  Uri? _initialURI;
-  Uri? _currentURI;
-  Object? _err;
-  StreamSubscription? _streamSubscription;
-
-  String _platformVersion = 'Unknown';
+class _MyHomePageState extends State<MyHomePage> {
   final _refluttersdkPlugin = Refluttersdk();
-  String token = "dEHA8nVPTq6wHFINot8wu-:APA91bHGLIoi2wp2tPmzMuVSqFtC3_KnB4lyXnzGhezi9MMwTiFzxrr1flBo_ltcgh0nI22QuXdStKk7W7mxA9MftHU15NmZKpcvRrcTXEUYAI1dkIpkqwzYpUl3jFKz8Bm9JDK-uf9m";
-  int notificationCount = 0,
-      _counter = 0;
-  late String cid;
-  late String nList;
-  TextEditingController controller1 = TextEditingController();
-
-
-  Future<void> setupInteractedMessage() async {
-    RemoteMessage? initialMessage =
-    await FirebaseMessaging.instance.getInitialMessage();
-
-    if (initialMessage != null) {
-     // getdeepLinkData();
-    }
-  }
-
+  late FirebaseMessaging messaging;
+  var token;
 
   @override
   void initState() {
     super.initState();
-    Firebase.initializeApp();
-     getdeepLinkData();
-    setupInteractedMessage();
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      // getdeepLinkData();
-    },);
     _getFcmToken();
     _refluttersdkPlugin.listener((data) {
       print("Deeplink Data $data");
     });
   }
+
   Future<void> _getFcmToken() async {
-    String? token = await FirebaseMessaging.instance.getToken();
-    print('Fcm Token: $token');
+    messaging = FirebaseMessaging.instance;
+    messaging.getToken().then((value) {
+      print('Fcm Token: $value');
+      token = value;
+    });
 
   }
 
@@ -75,64 +83,9 @@ class _MyAppState extends State<MyApp> {
     _refluttersdkPlugin.locationUpdate(lat, lang);
   }
 
-  newNotification() {
-    var notificationTitle = "sample Title";
-    var notificationBody = "sample Body";
-    //_refluttersdkPlugin.addNewNotification();
-  }
-
   customEvent() {
     var content = "On Track Event called!!!";
     _refluttersdkPlugin.customEvent(content);
-  }
-
-  deleteNotificationByCampaignid() {
-    setCidState();
-    _refluttersdkPlugin.deleteNotificationByCampaignId(cid);
-  }
-
-  setCidState() {
-    setState(() {
-      cid ="k2S||FaV|vV|T_tX7N6|1003123|Bulk|20221115060533";
-    });
-  }
-
-  readnotification() {
-    setCidState();
-    _refluttersdkPlugin.readNotification(cid);
-  }
-
-  unreadNotification() {
-    setCidState();
-    _refluttersdkPlugin.unReadNotification(cid);
-  }
-
-  appconversionTracking() {
-    _refluttersdkPlugin.appConversion();
-  }
-
-  appconversionTrackingWithData() {
-    Map appConvertionData={
-      "name":"xyrr",
-      "data":{
-        "age":"23",
-        "city":"yyy"
-      }
-    };
-    _refluttersdkPlugin.appConversionWithData(appConvertionData);
-  }
-
-  formdataCapture() {
-    Map formData = {
-      "Name": "vishwa",
-      "EmailID": "abc@gmail.com",
-      "MobileNo": 9329333,
-      "Gender": "Male",
-      "formid": 101, // required
-      "apikey": "api_key_b78db6rb3-9462-4132-a4d3-894db10b3782",
-      "City": "Chennai" // required
-    };
-    _refluttersdkPlugin.formDataCapture(formData);
   }
 
   customEventwithData() {
@@ -143,9 +96,51 @@ class _MyAppState extends State<MyApp> {
     _refluttersdkPlugin.customEventWithData(eventData);
   }
 
-  updatepushToken() {
-    _refluttersdkPlugin.updatePushToken(token);
+  deleteNotificationByCampaignid(campaignId) {
+    _refluttersdkPlugin.deleteNotificationByCampaignId(campaignId);
   }
+
+  readnotification(campaignId) {
+    _refluttersdkPlugin.readNotification(campaignId);
+  }
+
+  unreadNotification(campaignId) {
+    _refluttersdkPlugin.unReadNotification(campaignId);
+  }
+
+  appconversionTracking() {
+    _refluttersdkPlugin.appConversion();
+  }
+
+  appconversionTrackingWithData() {
+    Map appConversionData = {
+      "name":"xyrr",
+      "data":{
+        "age":"23",
+        "city":"yyy"
+      }
+    };
+    _refluttersdkPlugin.appConversionWithData(appConversionData);
+  }
+
+  formdataCapture() {
+
+    Map formData = {
+      "Name": "vishwa",
+      "EmailID": "abc@gmail.com",
+      "MobileNo": 9329333,
+      "Gender": "Male",
+      "formid": 101, // required
+      "apikey": "b78db6rb3-9462-4132-a4d3-894db10b3782",
+      "City": "Chennai" // required
+    };
+    _refluttersdkPlugin.formDataCapture(formData);
+  }
+
+  updatepushToken(fcmToken) {
+    _refluttersdkPlugin.updatePushToken(fcmToken);
+  }
+
   ondeviceRegister() {
 
     Map userData = {
@@ -166,22 +161,9 @@ class _MyAppState extends State<MyApp> {
     _refluttersdkPlugin.sdkRegisteration(userData);
   }
 
-
-  getdeepLinkData() {
-    print('getdeepLinkData 1::  Called');
-
-    _refluttersdkPlugin.deepLinkData();
-  }
-
-  deeplinkData (String data) {
-    print('onInstallDataReceived :: $data');
-  }
-
   readnotificationCount() async {
     var rnCount = await _refluttersdkPlugin.getReadNotificationCount();
-    setState(() {
-      notificationCount = rnCount!;
-    });
+
     if (kDebugMode) {
       print("readNotificationCount::$rnCount");
     }
@@ -199,11 +181,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        routes:{
-          '/phonefield' :(BuildContext contxt)=> Screen1(),
-          '/3vx5fd' :(BuildContext contxt)=> Screen2()
-        },
-
         home: Scaffold(
             appBar: AppBar(
               title: const Text('ReFlutter - Sdk'),
@@ -222,14 +199,8 @@ class _MyAppState extends State<MyApp> {
                           ),
                           SizedBox(
                             width: double.infinity,
-                            child: ElevatedButton(onPressed: () { //
-                              newNotification();
-                            }, child: Text("Add Notification")),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              updatepushToken();
+                              updatepushToken(token);
                             }, child: Text("update Push Token"),),
                           ),
                           SizedBox(
@@ -241,13 +212,13 @@ class _MyAppState extends State<MyApp> {
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              readnotification();
+                              readnotification("w39|G|qqD|iF|DEA5Q|430404|Bulk|20230418060124");
                             }, child: Text("Read Notification By Id"),),
                           ),
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              unreadNotification();
+                              unreadNotification("w39|G|qqD|iF|DEA5Q|430404|Bulk|20230418060124");
                             }, child: Text("UnRead Notification BY Id"),),
                           ),
                           SizedBox(
@@ -272,10 +243,7 @@ class _MyAppState extends State<MyApp> {
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              deleteNotificationByCampaignid();
-                              setState(() {
-                                controller1.clear();
-                              });
+                              deleteNotificationByCampaignid("w39|G|qqD|iF|DEA5Q|430404|Bulk|20230418060124");
                             }, child: Text("Delete Notification By CampaignId"),),
                           ),
                           SizedBox(
@@ -293,7 +261,6 @@ class _MyAppState extends State<MyApp> {
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              style:
                               ElevatedButton.styleFrom(
                                 minimumSize: Size.fromHeight(40),);
                               customEventwithData();
@@ -308,20 +275,10 @@ class _MyAppState extends State<MyApp> {
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(onPressed: () {
-                              style:
                               ElevatedButton.styleFrom(
                                 minimumSize: Size.fromHeight(40),);
                               appconversionTrackingWithData();
-                            }, child: Text("AppConvertionTrackingWithData"),),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: ElevatedButton(onPressed: () {
-                              style:
-                              ElevatedButton.styleFrom(
-                                minimumSize: Size.fromHeight(40),);
-                              getdeepLinkData();
-                            }, child: Text("GetDeepLinkData"),),
+                            }, child: Text("App Conversion Tracking WithData"),),
                           ),
                         ],
                       ),
@@ -331,49 +288,6 @@ class _MyAppState extends State<MyApp> {
         )
     );
   }
-
-
 }
 
 
-class Screen1 extends StatefulWidget {
-  const Screen1({Key? key}) : super(key: key);
-
-  @override
-  State<Screen1> createState() => _Screen1State();
-}
-
-class _Screen1State extends State<Screen1> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Column(
-        children: [
-          Text("Screen1")
-        ],
-      ),
-    );
-  }
-}
-
-class Screen2 extends StatefulWidget {
-  const Screen2({Key? key}) : super(key: key);
-
-  @override
-  State<Screen2> createState() => _Screen2State();
-}
-
-class _Screen2State extends State<Screen2> {
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-      color: Colors.blue,
-      child: Column(
-        children: [
-          Text("Screen2")
-        ],
-      ),
-    ); ;
-  }
-}
